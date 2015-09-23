441679f39b6e2814b7e74eab642cd9633648203a
Jelani <jelani.brandon@microsoft.com>
2015-09-23 10:23:27 -0700

    Standardizing Java Sample to WindSpeed

--- java/device/samples/send-serialized-event/src/main/java/samples/com/microsoft/azure/iothub/ContosoAnemometer.java
+++ java/device/samples/send-serialized-event/src/main/java/samples/com/microsoft/azure/iothub/ContosoAnemometer.java
@@ -5,10 +5,9 @@ package samples.com.microsoft.azure.iothub;
 
 import com.google.gson.Gson;
 
-public class ContosoThermostat505 {
-    public int temperature;
-    public int humidity;
-    public boolean LowTemperatureAlarm;
+public class ContosoAnemometer {
+    public String deviceId;
+    public double windSpeed;
 
     public boolean TurnFanOn() {
         System.out.println("Turning fan on.");
@@ -20,9 +19,8 @@ public class ContosoThermostat505 {
         return true;
     }
 
-    public boolean SetTemperature(int desiredTemperature) {
-        System.out.printf("Setting home temperature to %d degrees.\r\n", desiredTemperature);
-        this.temperature = desiredTemperature;
+    public boolean SetAirResistance(int position) {
+        System.out.printf("Setting Air Resistance Position to %d.\r\n", position);
         return true;
     }
 
