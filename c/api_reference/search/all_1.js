1edb7fbd5bd6217d9bc5685c94b1675515a78486
Damon Barry <damonb@microsoft.com>
2015-09-29 09:06:34 -0700

    Revert inadvertent merge of gh-pages to develop
    This reverts commit 0f6ddffd06c9f4468a50a9432406d3b031a52b55.

--- c/api_reference/search/all_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['base64_2eh',['base64.h',['../base64_8h.html',1,'']]],
-  ['base64_5fdecoder',['Base64_Decoder',['../base64_8h.html#af53129a184d122641b31220cbd401402',1,'base64.h']]],
-  ['base64_5fencode',['Base64_Encode',['../base64_8h.html#a3fc0095d207b2916b9af7e3b9bf25dbb',1,'base64.h']]],
-  ['base64_5fencode_5fbytes',['Base64_Encode_Bytes',['../base64_8h.html#a9e30a20c2abb2bf5e826426c953d12ed',1,'base64.h']]],
-  ['begin_5fnamespace',['BEGIN_NAMESPACE',['../serializer_8h.html#ad409cf8f57d6766ce80a504e679bb6a6',1,'serializer.h']]]
-];
