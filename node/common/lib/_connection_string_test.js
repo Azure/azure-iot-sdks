7a341321ed7da502073d42490871875a64c59691
Damon Barry <damonb@microsoft.com>
2015-10-09 11:30:26 -0700

    negative test case

--- node/common/lib/_connection_string_test.js
+++ node/common/lib/_connection_string_test.js
@@ -6,14 +6,19 @@
 var assert = require('chai').assert;
 
 function ConnectionString() {
-    this.segment = 'abc';
+    this.name = 'value';
 }
 
 describe('ConnectionString', function () {
   describe('#constructor', function () {
     it('finds the given segment in the connection string', function () {
-      var cn = new ConnectionString('segment=abc', 'segment');
-      assert.propertyVal(cn, 'segment', 'abc');
+      var cn = new ConnectionString('name=value', 'name');
+      assert.propertyVal(cn, 'name', 'value');
+    });
+
+    it('does not find the given segment if it is missing from the connection string', function () {
+      var cn = new ConnectionString('name=value', 'othername');
+      assert.notProperty('othername');
     });
   });
 });
