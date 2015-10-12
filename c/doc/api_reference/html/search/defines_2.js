6ab855ae8eab84a1e259fd12af96f5d4c2fac090
Rajasekharan Vengalil <rajave@microsoft.com>
2015-10-12 14:34:31 -0700

    Delete generated API docs from repo

--- c/doc/api_reference/html/search/defines_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['end_5fnamespace',['END_NAMESPACE',['../serializer_8h.html#af169ba5b3435392cf046bd3e07e324d3',1,'serializer.h']]],
-  ['execute_5fcommand',['EXECUTE_COMMAND',['../serializer_8h.html#a8b306a60e81bcfdf3729962df530ec16',1,'serializer.h']]]
-];
