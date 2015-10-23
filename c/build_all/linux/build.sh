cbdc382302d66850891abb4aadb7f6b50fd0ec74
Jelani <jelani.brandon@microsoft.com>
2015-10-23 14:08:22 -0700

    Update CMake file to exclude e2e test when not including mqtt

--- c/build_all/linux/build.sh
+++ c/build_all/linux/build.sh
@@ -66,7 +66,7 @@ process_args $*
 rm -r -f ~/cmake
 mkdir ~/cmake
 pushd ~/cmake
-cmake -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Duse_amqp:BOOL=$build_amqp -Duse_http:BOOL=$build_http -Duse_mqtt=$build_mqtt $build_root
+cmake -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Duse_amqp:BOOL=$build_amqp -Duse_http:BOOL=$build_http -Duse_mqtt:BOOL=$build_mqtt $build_root
 make --jobs=$(nproc)
 ctest -C "Debug" -V
 popd
