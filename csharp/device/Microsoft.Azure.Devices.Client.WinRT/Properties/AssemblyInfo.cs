bca01a7bb32691f4302ec664cb2b28171b583f60
tameraw <tameraw@microsoft.com>
2015-11-30 18:06:26 -0800

    Fixed delay signing for csharp device client.

--- csharp/device/Microsoft.Azure.Devices.Client.WinRT/Properties/AssemblyInfo.cs
+++ csharp/device/Microsoft.Azure.Devices.Client.WinRT/Properties/AssemblyInfo.cs
@@ -21,6 +21,11 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 
+#if (RELEASE_DELAY_SIGN)
+[assembly: AssemblyDelaySignAttribute(true)]
+[assembly: AssemblyKeyFileAttribute("35MSSharedLib1024.snk")]
+#endif
+
 // Version information for an assembly follows semantic versioning 1.0.0 (because
 // NuGet didn't support semver 2.0.0 before VS 2015). See semver.org for details.
 [assembly: AssemblyInformationalVersion("1.0.0-preview-008")]
--- csharp/device/Microsoft.Azure.Devices.Client/Properties/AssemblyInfo.cs
+++ csharp/device/Microsoft.Azure.Devices.Client/Properties/AssemblyInfo.cs
@@ -23,8 +23,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 
 // Product Friends
+#if (DEBUG || RELEASE)
 [assembly: InternalsVisibleTo("Microsoft.Azure.Devices.Client.Test")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+#endif
+
+#if (RELEASE_DELAY_SIGN)
+[assembly: AssemblyDelaySignAttribute(true)]
+[assembly: AssemblyKeyFileAttribute("35MSSharedLib1024.snk")]
+#endif
 
 // Version information for an assembly follows semantic versioning 1.0.0 (because
 // NuGet didn't support semver 2.0.0 before VS 2015). See semver.org for details.
