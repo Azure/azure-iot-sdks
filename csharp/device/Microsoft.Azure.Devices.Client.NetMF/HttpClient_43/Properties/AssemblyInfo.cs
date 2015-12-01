5ca176753544778f16530e642c7988ca6e51f477
José Simões <josesimoes@users.noreply.github.com>
2015-12-01 09:28:21 +0000

    Update AssemblyInfo.cs
    wrong title/name in assembly properties

--- csharp/device/Microsoft.Azure.Devices.Client.NetMF/HttpClient_43/Properties/AssemblyInfo.cs
+++ csharp/device/Microsoft.Azure.Devices.Client.NetMF/HttpClient_43/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Devices.Client.Http.NetMF_44")]
+[assembly: AssemblyTitle("Microsoft.Azure.Devices.Client.Http.NetMF_43")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Azure.Devices.Client.Http.NetMF_44")]
+[assembly: AssemblyProduct("Microsoft.Azure.Devices.Client.Http.NetMF_43")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
