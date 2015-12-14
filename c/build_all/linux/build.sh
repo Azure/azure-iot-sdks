0b60441f7abcfc159998f9e7fbd3aca3549a14e8
andrew-buckley <andrewbuckley@outlook.com>
2015-12-14 14:31:20 -0800

    Adds flag to skip unit tests on linux

--- c/build_all/linux/build.sh
+++ c/build_all/linux/build.sh
@@ -13,6 +13,7 @@ run_longhaul_tests=OFF
 build_amqp=ON
 build_http=ON
 build_mqtt=ON
+skip_unittests=OFF
 
 usage ()
 {
@@ -21,7 +22,8 @@ usage ()
     echo " -cl, --compileoption <value>  specify a compile option to be passed to gcc"
     echo "   Example: -cl -O1 -cl ..."
     echo " --skip-e2e-tests              skip the running of end-to-end tests (e2e tests are run by default)"
-    echo " --run-longhaul-tests          run long haul tests (long haul tests are not run by default)"
+    echo " --skip-unittests              skip the running of unit tests (unit tests are run by default)"
+	echo " --run-longhaul-tests          run long haul tests (long haul tests are not run by default)"
     echo ""
     echo " --no-amqp                     do no build AMQP transport and samples"
     echo " --no-http                     do no build HTTP transport and samples"
@@ -45,6 +47,7 @@ process_args ()
           case "$arg" in
               "-cl" | "--compileoption" ) save_next_arg=1;;
               "--skip-e2e-tests" ) run_e2e_tests=OFF;;
+			  "--skip-unittests" ) skip_unittests=ON;;
               "--run-longhaul-tests" ) run_longhaul_tests=ON;;
               "--no-amqp" ) build_amqp=OFF;;
               "--no-http" ) build_http=OFF;;
@@ -60,7 +63,7 @@ process_args $*
 rm -r -f ~/cmake
 mkdir ~/cmake
 pushd ~/cmake
-cmake -DcompileOption_C:STRING="$extracloptions" -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Duse_amqp:BOOL=$build_amqp -Duse_http:BOOL=$build_http -Duse_mqtt:BOOL=$build_mqtt $build_root
+cmake -DcompileOption_C:STRING="$extracloptions" -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Duse_amqp:BOOL=$build_amqp -Duse_http:BOOL=$build_http -Duse_mqtt:BOOL=$build_mqtt -Dskip_unittests:BOOL=$skip_unittests $build_root
 make --jobs=$(nproc)
 ctest -C "Debug" -V
 popd
