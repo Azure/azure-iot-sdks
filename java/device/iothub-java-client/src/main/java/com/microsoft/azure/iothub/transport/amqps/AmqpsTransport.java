de31fb730c7701b754fcf4e208c834651749f170
Zoltan Varga <zolvarga@microsoft.com>
2016-02-12 11:11:01 -0800

    Fix build error (random exception) for Java Client

--- java/device/iothub-java-client/src/main/java/com/microsoft/azure/iothub/transport/amqps/AmqpsTransport.java
+++ java/device/iothub-java-client/src/main/java/com/microsoft/azure/iothub/transport/amqps/AmqpsTransport.java
@@ -227,8 +227,13 @@ public final class AmqpsTransport implements IotHubTransport
                     }
                     //Instead of adding to callback packet, just invoke callbacks here in thread.
                     IotHubCallbackPacket callbackPacket = new IotHubCallbackPacket(status, packet.getCallback(), packet.getContext());
-                    IotHubEventCallback callback = callbackPacket.getCallback();
-                    callback.execute(callbackPacket.getStatus(), packet.getContext());
+                    if (callbackPacket != null) {
+                        IotHubEventCallback callback = callbackPacket.getCallback();
+                        if (callback != null) {
+                            callback.execute(callbackPacket.getStatus(), packet.getContext());
+                        }
+                    }
+
                 }).start();
             }
 
