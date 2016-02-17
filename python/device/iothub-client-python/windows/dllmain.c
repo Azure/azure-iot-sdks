39c02140f0377256b4d82b1cf43c7e403af34ab7
Martin Regen <mregen@microsoft.com>
2016-02-17 10:56:19 +0100

    clean up dllmain

--- python/device/iothub-client-python/windows/dllmain.c
+++ python/device/iothub-client-python/windows/dllmain.c
@@ -1,16 +1,6 @@
 // dllmain.c : Defines the entry point for the DLL application.
 #include <windows.h>
 
-/* This sample uses the _LL APIs of iothub_client for example purposes.
-That does not mean that HTTP only works with the _LL APIs.
-Simply changing the using the convenience layer (functions not having _LL)
-and removing calls to _DoWork will yield the same results. */
-
-#include "iothub_client_ll.h"
-#include "iothub_message.h"
-#include "crt_abstractions.h"
-#include "iothubtransporthttp.h"
-
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
