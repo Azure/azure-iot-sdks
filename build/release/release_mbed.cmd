5ad9938d77f6d133559d4a3f71ba01e37d653955
anthonyvercolano <toerocla@microsoft.com>
2016-04-04 10:15:21 -0700

    Update to new include structure.

--- build/release/release_mbed.cmd
+++ build/release/release_mbed.cmd
@@ -10,7 +10,7 @@ call release_mbed_project.cmd ..\..\..\c\iothub_client\build\iothub_client
 call release_mbed_project.cmd ..\..\..\c\azure-uamqp-c\build
 call release_mbed_project.cmd ..\..\..\c\azure-umqtt-c\build
 call release_mbed_project.cmd ..\..\..\c\serializer\build
-call release_mbed_project.cmd ..\..\..\c\azure-c-shared-utility\c\build
+call release_mbed_project.cmd ..\..\..\c\azure-c-shared-utility\build
 call release_mbed_project.cmd ..\..\..\c\iothub_client\samples\iothub_client_sample_amqp
 call release_mbed_project.cmd ..\..\..\c\iothub_client\samples\iothub_client_sample_http
 call release_mbed_project.cmd ..\..\..\c\iothub_client\samples\iothub_client_sample_mqtt
@@ -24,10 +24,10 @@ call release_mbed_project.cmd ..\..\..\c\iothub_client\build\iothub_amqp_transpo
 call release_mbed_project.cmd ..\..\..\c\iothub_client\build\iothub_http_transport iothub_http_transport_bld
 call release_mbed_project.cmd ..\..\..\c\iothub_client\build\iothub_mqtt_transport iothub_mqtt_transport_bld
 call release_mbed_project.cmd ..\..\..\c\iothub_client\build\iothub_client iothub_client_bld
-call release_mbed_project.cmd ..\..\..\c\azure-uamqp-c\build azure_uamqp_bld
-call release_mbed_project.cmd ..\..\..\c\azure-umqtt-c\build azure_umqtt_bld
+call release_mbed_project.cmd ..\..\..\c\azure-uamqp-c\build azure_uamqp_c_bld
+call release_mbed_project.cmd ..\..\..\c\azure-umqtt-c\build azure_umqtt_c_bld
 call release_mbed_project.cmd ..\..\..\c\serializer\build serializer_bld
-call release_mbed_project.cmd ..\..\..\c\azure-c-shared-utility\c\build azureiot_common_bld
+call release_mbed_project.cmd ..\..\..\c\azure-c-shared-utility\build azure_c_shared_utility_bld
 call release_mbed_project.cmd ..\..\..\c\iothub_client\samples\iothub_client_sample_amqp iothub_client_sample_amqp_bld
 call release_mbed_project.cmd ..\..\..\c\iothub_client\samples\iothub_client_sample_http iothub_client_sample_http_bld
 call release_mbed_project.cmd ..\..\..\c\iothub_client\samples\iothub_client_sample_mqtt iothub_client_sample_mqtt_bld
