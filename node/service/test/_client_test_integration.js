c305af165b95321fa9837d36e0af5a3583c8a230
Pierre Cauchois <pierreca@microsoft.com>
2016-04-15 15:52:19 -0700

    Test and requirement docs update following PR #488 (Add AMQP-WS support to node.js service SDK)

--- node/service/test/_client_test_integration.js
+++ node/service/test/_client_test_integration.js
@@ -2,13 +2,24 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 'use strict';
+var AmqpWs = require('../lib/amqp_ws.js');
 
 var transportSpecificTests = require('./_client_common_testrun.js');
 
-describe('Over real AMQP', function () {
+describe('Over real AMQP (Default Transport)', function () {
   this.timeout(60000);
   var opts = {
     transport: null,
+    connectionString: process.env.IOTHUB_CONNECTION_STRING,
+    id: process.env.IOTHUB_DEVICE_ID
+  };
+  transportSpecificTests(opts);
+});
+
+describe('Over real AMQP over Websockets', function () {
+  this.timeout(60000);
+  var opts = {
+    transport: AmqpWs,
     connectionString: process.env.IOTHUB_CONNECTION_STRING,
     id: process.env.IOTHUB_DEVICE_ID
   };
