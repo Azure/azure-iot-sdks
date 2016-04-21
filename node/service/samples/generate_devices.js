aafff1053844c8275613c919d080ef5e0c9cc388
Pierre Cauchois <pierreca@microsoft.com>
2016-04-20 19:31:17 -0700

    Node.js service samples and readme adjustments

--- node/service/samples/generate_devices.js
+++ node/service/samples/generate_devices.js
@@ -5,13 +5,13 @@
 
 var iothub = require('azure-iothub');
 var fs = require('fs');
-var common = require('./common.js');
+var utilities = require('./utilities.js');
 
 // This is the input file that should be in the same folder as this sample, and contain the connection string to the IoT Hub service.
 var connectionString = '[IoT Hub Connection String]';
 var hostName = iothub.ConnectionString.parse(connectionString).HostName;
 
-common.loadDeviceData(function(err, deviceData) {
+utilities.loadDeviceData(function(err, deviceData) {
   // Step 1: create the registry object, that will be used to create devices
   var registry = iothub.Registry.fromConnectionString(connectionString);
   
@@ -24,7 +24,7 @@ common.loadDeviceData(function(err, deviceData) {
         console.log('device created: ' + result.deviceId);
         // Step 3: the device creation has succeeded. Generate the connection string and append it to the file containing device credentials for use with other samples.
         var deviceConnStr = "HostName=" + hostName + ';DeviceId=' + result.deviceId + ';SharedAccessKey=' + result.authentication.symmetricKey.primaryKey;
-        fs.appendFile(common.deviceCredentialsFile, deviceConnStr + '\r\n', function (err) {
+        fs.appendFile(utilities.deviceCredentialsFile, deviceConnStr + '\r\n', function (err) {
           if (err) {
             console.error('Could not write to device credentials file');
           }
