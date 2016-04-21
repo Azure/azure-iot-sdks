7e84f874cbd62e5277eace1451f710fa4d97fddd
Martin Regen <mregen@microsoft.com>
2016-04-21 15:11:24 +0200

    rename and simplify Python samples, revert DeviceExplorer edit

--- python/device/samples/iothub_client_sample_class.py
+++ python/device/samples/iothub_client_sample_class.py
@@ -11,7 +11,7 @@ import iothub_client
 from iothub_client import *
 
 receiveContext = 0
-avgWindSpeed = 20.0
+avgWindSpeed = 10.0
 message_count = 5
 received_count = 0
 
@@ -23,7 +23,7 @@ send_callbacks = 0
 # "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
 connectionString = "[device connection string]"
 
-msgTxt = "{\"deviceId\": \"myAMQPDevice\",\"windSpeed\": %.2f}"
+msgTxt = "{\"deviceId\": \"myPythonDevice\",\"windSpeed\": %.2f}"
 
 
 class HubManager(object):
@@ -43,35 +43,35 @@ class HubManager(object):
         from iothub_client_cert import certificates
         try:
             self.client.set_option("TrustedCerts", certificates)
-            print ("set_option TrustedCerts successful")
+            print("set_option TrustedCerts successful")
         except IoTHubClientError as e:
-            print ("set_option TrustedCerts failed (%s)" % e)
+            print("set_option TrustedCerts failed (%s)" % e)
 
     def _receive_message_callback(self, message, counter):
         global receive_callbacks
         buffer = message.get_bytearray()
         size = len(buffer)
-        print ("Received Message [%d]:" % counter)
-        print ("    Data: <<<%s>>> & Size=%d" %
-               (buffer[:size].decode('utf-8'), size))
+        print("Received Message [%d]:" % counter)
+        print("    Data: <<<%s>>> & Size=%d" %
+              (buffer[:size].decode('utf-8'), size))
         mapProperties = message.properties()
         keyValuePair = mapProperties.get_internals()
-        print ("    Properties: %s" % keyValuePair)
+        print("    Properties: %s" % keyValuePair)
         counter += 1
         receive_callbacks += 1
-        print ("    Total calls received: %d" % receive_callbacks)
+        print("    Total calls received: %d" % receive_callbacks)
         return IoTHubMessageDispositionResult.ACCEPTED
 
     def _send_confirmation_callback(self, message, result, userContext):
         global send_callbacks
-        print (
+        print(
             "Confirmation[%d] received for message with result = %s" %
             (userContext, result))
         mapProperties = message.properties()
         keyValuePair = mapProperties.get_internals()
-        print ("    Properties: %s" % keyValuePair)
+        print("    Properties: %s" % keyValuePair)
         send_callbacks += 1
-        print ("    Total calls confirmed: %d" % send_callbacks)
+        print("    Total calls confirmed: %d" % send_callbacks)
 
     def send_event(self, event, properties, send_context):
         if not isinstance(event, IoTHubMessage):
@@ -88,20 +88,20 @@ class HubManager(object):
 
 def main():
     try:
-        print ("\nPython %s\n" % sys.version)
-        print (
+        print("\nPython %s\n" % sys.version)
+        print(
             "IoT Hub for Python SDK Version: %s\n" %
             iothub_client.__version__)
 
         hub_manager = HubManager(connectionString)
 
-        print (
+        print(
             "Starting the IoT Hub Python sample using protocol %s..." %
             hub_manager.client_protocol)
 
         while True:
             # send a few messages every minute
-            print ("IoTHubClient sending %d messages" % message_count)
+            print("IoTHubClient sending %d messages" % message_count)
 
             for i in range(0, message_count):
                 msgTxtFormatted = msgTxt % (
@@ -110,24 +110,25 @@ def main():
                     "Property": "PropMsg_%d" % i
                 }
                 hub_manager.send_event(msgTxtFormatted, msg_properties, i)
-                print (
-                    "IoTHubClient.send_event_async accepted message [%d] for transmission to IoT Hub." %
+                print(
+                    "IoTHubClient.send_event_async accepted message [%d]"
+                    " for transmission to IoT Hub." %
                     i)
 
             # Wait for Commands or exit
-            print ("IoTHubClient waiting for commands, press Ctrl-C to exit")
+            print("IoTHubClient waiting for commands, press Ctrl-C to exit")
 
             n = 0
             while n < 6:
                 status = hub_manager.client.get_send_status()
-                print ("Send status: %s" % status)
+                print("Send status: %s" % status)
                 time.sleep(10)
                 n += 1
     except IoTHubError as e:
-        print ("Unexpected error %s from IoTHub" % e)
+        print("Unexpected error %s from IoTHub" % e)
         return
     except KeyboardInterrupt:
-        print ("IoTHubClient sample stopped")
+        print("IoTHubClient sample stopped")
 
 
 if __name__ == '__main__':
