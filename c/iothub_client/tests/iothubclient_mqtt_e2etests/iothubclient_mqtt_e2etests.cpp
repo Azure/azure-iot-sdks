d6ce58d3d469ab1c1f22bf47d5c41224a0f1cdfb
Bert Kleewein <bertk@microsoft.com>
2016-05-12 11:48:10 -0700

    comment fix

--- c/iothub_client/tests/iothubclient_mqtt_e2etests/iothubclient_mqtt_e2etests.cpp
+++ c/iothub_client/tests/iothubclient_mqtt_e2etests/iothubclient_mqtt_e2etests.cpp
@@ -1,4 +1,4 @@
-// cOpyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <cstdlib>
