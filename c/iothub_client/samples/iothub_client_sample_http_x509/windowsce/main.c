385631edf925d4230714e0b9c2221bb03ecc1bda
Andrei Porumb <anporumb@microsoft.com>
2016-06-23 21:24:32 -0700

    added a HTTP x509 sample

---
+++ c/iothub_client/samples/iothub_client_sample_http_x509/windowsce/main.c
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include "iothub_client_sample_http_x509.h"
+
+int main(void)
+{
+    iothub_client_sample_http_x509_run();
+    return 0;
+}
