5a0efecf7a86aff4bd0ff5f12d9acf0cb78b79b0
Pierre Cauchois <pierreca@microsoft.com>
2016-08-11 16:57:13 -0700

    [node] Add build script options for E2E tests

--- node/build/build.cmd
+++ node/build/build.cmd
@@ -14,11 +14,13 @@ rem ---------------------------------------------------------------------------
 
 set min-output=0
 set integration-tests=0
+set e2e-tests=0
 
 :args-loop
 if "%1" equ "" goto args-done
 if "%1" equ "--min" goto arg-min-output
 if "%1" equ "--integration-tests" goto arg-integration-tests
+if "%1" equ "--e2e-tests" goto arg-e2e-tests
 call :usage && exit /b 1
 
 :arg-min-output
@@ -27,6 +29,10 @@ goto args-continue
 
 :arg-integration-tests
 set integration-tests=1
+goto args-continue
+
+:arg-e2e-tests
+set e2e-tests=1
 goto args-continue
 
 :args-continue
@@ -87,8 +93,10 @@ if errorlevel 1 goto :cleanup
 call :lint-and-test %node-root%\service
 if errorlevel 1 goto :cleanup
 
-call :lint-and-test %node-root%\e2etests
-if errorlevel 1 goto :cleanup
+if %e2e-tests%==1 (
+  call :lint-and-test %node-root%\e2etests
+  if errorlevel 1 goto :cleanup
+)
 
 cd %node-root%\..\tools\iothub-explorer
 call npm -s test
@@ -107,6 +115,7 @@ echo build.cmd [options]
 echo options:
 echo  --min                 minimize display output
 echo  --integration-tests   run integration tests too (unit tests always run)
+echo  --e2e-tests           run end-to-end tests too (unit tests always run)
 goto :eof
 
 :lint-and-test
