d354a88f9b4130ec0b9ad8866b322db75f17aa40
jasminel <jasminel@microsoft.com>
2016-08-31 10:07:55 -0700

    code review rework

--- build/release/installPackage.ps1
+++ build/release/installPackage.ps1
@@ -8,11 +8,11 @@ $mqttTransportPkg = Get-ChildItem $path\Microsoft.Azure.IoTHub.MqttTransport.*.n
 $serializerPkg = Get-ChildItem $path\Microsoft.Azure.IoTHub.Serializer.*.nupkg
 $uamqpPkg = Get-ChildItem $path\Microsoft.Azure.uamqp.*.nupkg
 
-Install-Package $($path+ '\'+($amqpTransportPkg.Name)) -ProjectName iothubclient_amqp_e2e_nuget_exe
-Install-Package $($path+ '\'+($mqttTransportPkg.Name)) -ProjectName iothubclient_mqtt_e2e_nuget_exe
-Install-Package $($path+ '\'+($httpTransportPkg.Name)) -ProjectName iothubclient_http_e2e_nuget_exe
-Install-Package $($path+ '\'+($uamqpPkg.Name)) -ProjectName iothubclient_http_e2e_nuget_exe
-Install-Package $($path+ '\'+($serializerPkg.Name)) -ProjectName serializer_e2e_nuget_exe
-Install-Package $($path+ '\'+($amqpTransportPkg.Name)) -ProjectName serializer_e2e_nuget_exe
-Install-Package $($path+ '\'+($httpTransportPkg.Name)) -ProjectName serializer_e2e_nuget_exe
-Install-Package $($path+ '\'+($uamqpPkg.Name)) -ProjectName iothubclient_mqtt_e2e_nuget_exe
+Install-Package $($path + '\' + ($amqpTransportPkg.Name)) -ProjectName iothubclient_amqp_e2e_nuget_exe
+Install-Package $($path + '\' + ($mqttTransportPkg.Name)) -ProjectName iothubclient_mqtt_e2e_nuget_exe
+Install-Package $($path + '\' + ($httpTransportPkg.Name)) -ProjectName iothubclient_http_e2e_nuget_exe
+Install-Package $($path + '\' + ($uamqpPkg.Name)) -ProjectName iothubclient_http_e2e_nuget_exe
+Install-Package $($path + '\' + ($serializerPkg.Name)) -ProjectName serializer_e2e_nuget_exe
+Install-Package $($path + '\' + ($amqpTransportPkg.Name)) -ProjectName serializer_e2e_nuget_exe
+Install-Package $($path + '\' + ($httpTransportPkg.Name)) -ProjectName serializer_e2e_nuget_exe
+Install-Package $($path + '\' + ($uamqpPkg.Name)) -ProjectName iothubclient_mqtt_e2e_nuget_exe
