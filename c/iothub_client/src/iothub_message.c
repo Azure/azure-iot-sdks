d640835005d32b4cb624e40ea931d9f7306f17f7
ewertons <ewertons@microsoft.com>
2016-09-07 11:09:03 -0700

    Reverting incorrect include on iothub_message for uamqp_messaging work

--- c/iothub_client/src/iothub_message.c
+++ c/iothub_client/src/iothub_message.c
@@ -8,7 +8,6 @@
 #include "azure_c_shared_utility/gballoc.h"
 #include "azure_c_shared_utility/xlogging.h"
 #include "azure_c_shared_utility/buffer_.h"
-#include "azure_uamqp_c/message.h"
 
 #include "iothub_message.h"
 
