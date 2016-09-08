a40af20821694ff1980b664a2221fb9bfc70776a
dcristoloveanu <dcristo@microsoft.com>
2016-09-07 22:20:28 -0700

    Re-enable E2E tests for Linux

--- jenkins/ubuntu1510_c.sh
+++ jenkins/ubuntu1510_c.sh
@@ -6,6 +6,6 @@ build_root=$(cd "$(dirname "$0")/.." && pwd)
 cd $build_root
 
 # -- C --
-./c/build_all/linux/build.sh --run_valgrind "$@" #-x 
+./c/build_all/linux/build.sh --run_valgrind --run-e2e-tests "$@" #-x 
 [ $? -eq 0 ] || exit $?
 
