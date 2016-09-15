e355e69555da5fd0e4615ee73c058c94a563fd81
Bert Kleewein <bertk@microsoft.com>
2016-09-15 10:37:36 -0700

    sample update

--- node/device/samples/simple_sample_device_twin.js
+++ node/device/samples/simple_sample_device_twin.js
@@ -5,6 +5,7 @@
 
 var Client = require('azure-iot-device').Client;
 var Protocol = require('azure-iot-device-mqtt').Mqtt;
+var _ = require('lodash');
 
 var connectionString = process.argv[2];
 
@@ -26,6 +27,22 @@ client.open(function(err) {
       } else {
         console.log('twin created');
 
+        // First, set up some helper functions to deal with desired
+        // property changes.  We put these into individual functions because
+        // we can get new properties in two ways:
+        //
+        // 1) When we first connect, we get initial property values from the
+        //    service.  These could be pre-populated values, or they could be
+        //    values from a previous instance of this device (before a device
+        //    reboot, for example).
+        //
+        // 2) In response to a property change while this app is running.
+        //    In  this case, the service sends an update to the connected
+        //    device and the twin library converts that to an event that we
+        //    can listen for.
+        //
+
+
         // There are 4 examples here.  The app developer has the option to chose
         // which sample style to use (or mix and match).  All of the events 
         // shown here will fire and it's up to the app developer to decide what
@@ -42,11 +59,11 @@ client.open(function(err) {
         // This code will output any properties that are received from the 
         // service.
         //
-        twin.on('properties.desired', function(delta) {
-          console.log('PATCH received:');
-          console.log(JSON.stringify(delta));
-        });
-
+        var setNewDesiredState = function(delta) {
+            console.log('new desired properties received:');
+            console.log(JSON.stringify(delta));
+        };
+        
         // Usage example #2: receiving an event if anything under 
         // properties.desired.climate changes
         //
@@ -61,24 +78,24 @@ client.open(function(err) {
         //    }
         //  });
         //
-        twin.on('properties.desired.climate', function(delta) {
-          //
-          // Notice that twin.properties.desired has already been updated before
-          // this function was called.
-          //
-          // If the delta contains a minTemp or a maxTemp, then update the
-          // hardware with the values stored in the twin.  The twin has all the
-          // same values as the delta, but the delta only has values that
-          // have changed.  When we need the "current values", we don't use the
-          // values stored in the delta because it's possible that the delta has
-          // one or the other, but not both.
-          //
-          if (delta.minTemperature || delta.maxTemperature) {
-            console.log('updating desired temp:');
-            console.log('min temp = ' + twin.properties.desired.climate.minTemperature);
-            console.log('max temp = ' + twin.properties.desired.climate.maxTemperature);
-          }
-        });
+        var setNewClimateValues = function(delta) {
+            //
+            // Notice that twin.properties.desired has already been updated before
+            // this function was called.
+            //
+            // If the delta contains a minTemp or a maxTemp, then update the
+            // hardware with the values stored in the twin.  The twin has all the
+            // same values as the delta, but the delta only has values that
+            // have changed.  When we need the "current values", we don't use the
+            // values stored in the delta because it's possible that the delta has
+            // one or the other, but not both.
+            //
+            if (delta.minTemperature || delta.maxTemperature) {
+              console.log('updating desired temp:');
+              console.log('min temp = ' + twin.properties.desired.climate.minTemperature);
+              console.log('max temp = ' + twin.properties.desired.climate.maxTemperature);
+            }
+        };
 
         // Usage example #3: receiving an event for a single (scalar) property
         // value.  This event is only fired if the fanOn boolean value is part
@@ -97,10 +114,9 @@ client.open(function(err) {
         //      }
         //    }
         //  });
-
-        twin.on('properties.desired.climate.hvac.systemControl.fanOn', function(fanOn) {
-          console.log('setting fan state to ' + fanOn);
-        });
+        var setFanState = function(fanOn) {
+            console.log('setting fan state to ' + fanOn);
+        };
 
         // Usage example #4: handle add or delete operations.  The app developer
         // is responsible for inferring add/update/delete operations based on 
@@ -131,14 +147,14 @@ client.open(function(err) {
         //    }
         //  });
         //
-
+        
         // To do this, first we have to keep track of "all modules that we know
         // about".
         var moduleList = {};
 
-        // Then we add a handler for the parent of the properties that we care
-        // about.
-        twin.on('properties.desired.modules', function(delta) {
+        // Then we use this internal list and compare it to the delta to know
+        // if anything was added, removed, or updated.
+        var updateModules = function(delta) {
           Object.keys(delta).forEach(function(key) {
             
             if (delta[key] === null && moduleList[key]) {
@@ -166,7 +182,30 @@ client.open(function(err) {
               }
             }
           });
-        });
+        };
+
+        // At this point, the twin has received the initial values from the
+        // service.  Handle setting any initial state.  Use _.get to verify
+        // that the values actually exist before we actually call the helpers.
+        if (_.get(twin, 'properties.desired')) {
+          setNewDesiredState(twin.properties.desired);
+        }
+        if (_.get(twin,'properties.desired.climate')) {
+          setNewClimateValues(twin.properties.desired.climate);
+        }
+        if (_.get(twin, 'properties.desired.climate.hvac.systemControl.fanOn')) {
+          setFanState(this.properties.desired.climate.hvac.systemControl.fanOn);
+        }
+        if (_.get(twin, 'properties.desired.modules')) {
+          updateModules(twin.properties.desired.modules);
+        }
+
+        // Then add handlers for any deltas that may arrive now that we're 
+        // running 
+        twin.on('properties.desired', setNewDesiredState);
+        twin.on('properties.desired.climate', setNewClimateValues);
+        twin.on('properties.desired.climate.hvac.systemControl.fanOn', setFanState);
+        twin.on('properties.desired.modules', updateModules);
 
         // create a patch to send to the hub
         var patch = {
