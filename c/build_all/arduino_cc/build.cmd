bebaf651c725f2f9ee5a1a011dae4bf9fbdccb3e
mamokarz <mamokarz@microsoft.com>
2016-10-06 15:30:09 -0700

    Add the script to upload and test a new version on Arduino Huzzah and fix issues

--- c/build_all/arduino_cc/build.cmd
+++ c/build_all/arduino_cc/build.cmd
@@ -29,7 +29,7 @@ rem ----------------------------------------------------------------------------
 rem -- parse script arguments
 rem -----------------------------------------------------------------------------
 set build_clean=OFF
-set build_test=OFF
+set build_test=ON
 set run_test=OFF
 set clone_test=ON
 
@@ -40,7 +40,9 @@ if "%1" equ "--clean" goto arg_build_clean
 if "%1" equ "--root" goto arg_test_root
 if "%1" equ "-b" goto arg_build_only
 if "%1" equ "--build-only" goto arg_build_only
-if "%1" equ "-r" goto arg_run_e2e_tests
+if "%1" equ "-r" goto arg_run_only
+if "%1" equ "--run-only" goto arg_run_only
+if "%1" equ "-e2e" goto arg_run_e2e_tests
 if "%1" equ "--run-e2e-tests" goto arg_run_e2e_tests
 call :usage && exit /b 1
 
@@ -56,6 +58,11 @@ goto args_continue
 
 :arg_build_only
 set build_test=ON
+goto args_continue
+
+:arg_run_only
+set build_test=OFF
+set run_test=ON
 goto args_continue
 
 :arg_run_e2e_tests
@@ -153,32 +160,47 @@ call download_blob.cmd -directory %user_packages_path% -file adafruit -check har
 call download_blob.cmd -directory %user_packages_path% -file arduino -check hardware\samd\1.6.6\libraries
 
 rem -----------------------------------------------------------------------------
-rem -- build solution
+rem -- Execute all tests
 rem -----------------------------------------------------------------------------
 
-if %build_test%==ON (
-    call :BuildAllTests
-)
-
-popd
-
-echo.
-echo.
-echo Build results:
+call :ExecuteAllTests
 
 set finalResult=0
-for /F "tokens=2* delims=.=" %%A in ('set __errolevel_build.') do (
-    if not "%%B"=="0" (
-        set finalResult=1
-        @echo Build %%A !!!FAILED!!!
-    ) else (
-        @echo Build %%A SUCCEED
+
+if %build_test%==ON (
+    echo.
+    echo.
+    echo Build results:
+
+    for /F "tokens=2* delims=.=" %%A in ('set __errolevel_build.') do (
+        @echo Build %%A %%B
+        if "%%B"=="FAILED" (
+            set finalResult=1
+        )
     )
 )
 
-if not !finalResult!==0 exit /b !finalResult!
+if %run_test%==ON (
+    echo.
+    echo.
+    echo Execution results:
 
-goto :eof
+    for /F "tokens=2* delims=.=" %%A in ('set __errolevel_run.') do (
+        @echo Run %%A %%B
+        if "%%B"=="FAILED" (
+            set finalResult=1
+        )
+    )
+)
+
+echo.
+if !finalResult!==0 (
+   echo TEST SUCCEED
+) else (
+   echo TEST FAILED
+)
+
+exit /b !finalResult!
 
 
 rem -----------------------------------------------------------------------------
@@ -188,10 +210,11 @@ rem ----------------------------------------------------------------------------
 :usage
 echo build.cmd [options]
 echo options:
-echo  -c, --clean           delete artifacts from previous build before building
-echo  -b, --build-only      only build the project
-echo  -r, --run-test        run end-to-end test
-echo  --root <test_path>    determine the root of the test tree
+echo  -c, --clean             delete artifacts from previous build before building
+echo  -b, --build-only        only build the project (default)
+echo  -r, --run-only          only run the test
+echo  -e2e, --run-e2e-tests   run end-to-end test
+echo  --root <test_path>      determine the root of the test tree
 goto :eof
 
 
@@ -200,7 +223,7 @@ rem -- helper subroutines
 rem -----------------------------------------------------------------------------
 
 REM Parser tests.lst
-:BuildAllTests
+:ExecuteAllTests
 set buildlog=tests.lst
 set testName=false
 set projectName=
@@ -213,7 +236,12 @@ for /F "tokens=*" %%F in (%buildlog%) do (
         if /i "!command:~0,1!"=="#" (
             echo Comment=!command:~1!
         ) else ( if /i "!command:~0,1!"=="[" (
-            call :BuildTest
+            if %build_test%==ON (
+                call :BuildTest
+            )
+            if %run_test%==ON (
+                call :RunTest
+            )
             set projectName=!command:~1,-1!
             if /i "!projectName!"=="End" goto :eof
         ) else (
@@ -227,11 +255,12 @@ for /F "tokens=*" %%F in (%buildlog%) do (
 goto :eof
 
 
-REM Execute each test in the Tests.lst
+REM Build each test in the Tests.lst
 :BuildTest
 if not "!projectName!"=="" (
     echo.
     echo Build !projectName!
+    echo   Target=!Target!
     echo   RelativePath=!RelativePath!
     echo   RelativeWorkingDir=!RelativeWorkingDir!
     echo   MaxAllowedDurationSeconds=!MaxAllowedDurationSeconds!
@@ -239,10 +268,9 @@ if not "!projectName!"=="" (
     echo   Hardware=!Hardware!
     echo   CPUParameters=!CPUParameters!
     echo   Build=!Build!
-    echo   Installation=!Installation!
-    echo   Execution=!Execution!
 
     if "!Build!"=="Disable" (
+        set __errolevel_build.!projectName!=DISABLED
         echo ** Build for !projectName! is disable. **
         goto :eof
     )
@@ -294,7 +322,7 @@ rem                     "F:\Azure\IoT\SDKs\iot-hub-c-huzzah-getstartedkit-master
     set hardware_parameters=-hardware "%compiler_hardware_path%" -hardware "%user_hardware_path%" -hardware "%user_packages_path%" -hardware "%user_libraries_path%"
     set tools_parameters=-tools "%compiler_tools_builder_path%" -tools "%compiler_tools_processor_path%" -tools "%user_packages_path%"
     set libraries_parameters=-built-in-libraries "%compiler_libraries_path%" -libraries "%user_libraries_path%"
-    set parameters=-logger=machine !hardware_parameters! !tools_parameters! !libraries_parameters! !CPUParameters! -build-path "%build_root%!RelativeWorkingDir!" -warnings=none -prefs=build.warn_data_percentage=75 -verbose %test_root%!RelativePath!
+    set parameters=-logger=machine !hardware_parameters! !tools_parameters! !libraries_parameters! !CPUParameters! -build-path "%build_root%!RelativeWorkingDir!" -warnings=none -prefs=build.warn_data_percentage=75 -verbose %test_root%!RelativePath!\!Target!
 
     echo Dump Arduino preferences:
     echo  !compiler_name! -dump-prefs !parameters!
@@ -304,7 +332,39 @@ rem                     "F:\Azure\IoT\SDKs\iot-hub-c-huzzah-getstartedkit-master
     echo  !compiler_name! -compile !parameters!
     call !compiler_name! -compile !parameters!
 
-    set __errolevel_build.!projectName!=%errorlevel%
+    if "%errorlevel%"=="0" (
+        set __errolevel_build.!projectName!=SUCCEED
+    ) else (
+        set __errolevel_build.!projectName!=FAILED
+    )
+)
+goto :eof
+
+
+REM Run each test in the Tests.lst
+:RunTest
+if not "!projectName!"=="" (
+    echo.
+    echo Run !projectName!
+    echo   Target=!Target!
+    echo   RelativeWorkingDir=!RelativeWorkingDir!
+    echo   LogLines=!LogLines!
+    echo   MinimumHeap=!MinimumHeap!
+    echo   Execution=!Execution!
+
+    if "!Execution!"=="Disable" (
+        set __errolevel_run.!projectName!=DISABLED
+        echo ** Execution for !projectName! is disable. **
+        goto :eof
+    )
+    
+    call powershell.exe -NoProfile -NonInteractive -ExecutionPolicy unrestricted -Command .\execute.ps1 -binaryPath:%build_root%!RelativeWorkingDir!\!Target!.bin -serialPort:COM4 -esptool:%build_root%\arduino15\packages\esp8266\tools\esptool\0.4.8\esptool.exe -logLines:!LogLines! -minimumHeap:!MinimumHeap!
+
+    if "!errorlevel!"=="0" (
+        set __errolevel_run.!projectName!=SUCCEED
+    ) else (
+        set __errolevel_run.!projectName!=FAILED
+    )
 
 )
 goto :eof
