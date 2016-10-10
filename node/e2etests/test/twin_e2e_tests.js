feb1e864154fbef3df5c42e284cdde5b46c73145
Bert Kleewein <bertk@microsoft.com>
2016-10-10 10:32:32 -0700

    twin e2e tests

--- node/e2etests/test/twin_e2e_tests.js
+++ node/e2etests/test/twin_e2e_tests.js
@@ -5,9 +5,7 @@
 
 var Registry = require('azure-iothub').Registry;
 var ConnectionString = require('azure-iothub').ConnectionString;
-var anHourFromNow = require('azure-iot-common').anHourFromNow;
 var deviceSdk = require('azure-iot-device');
-var deviceSas = deviceSdk.SharedAccessSignature;
 var deviceMqtt = require('azure-iot-device-mqtt').Mqtt;
 var uuid = require('uuid');
 var _ = require('lodash');
@@ -34,24 +32,29 @@ var mergeResult =  _.merge(JSON.parse(JSON.stringify(newProps)), moreNewProps);
 var runTests = function (hubConnectionString) {
   describe('Twin', function() {
 
-    this.timeout(120000);
+    this.timeout(10000);
     var deviceClient, deviceTwin;
     var serviceTwin;
 
     var deviceDescription;
 
     beforeEach(function (done) {
-      this.timeout(20000);
+      this.timeout(10000);
 
+      var host = ConnectionString.parse(hubConnectionString).HostName;
+      var pkey = new Buffer(uuid.v4()).toString('base64');
+      var deviceId = '0000e2etest-delete-me-twin-e2e-' + uuid.v4();
+      
       deviceDescription = {
-        deviceId:  '0000e2etest-delete-me-twin-e2e-' + uuid.v4(),
+        deviceId:  deviceId,
         status: 'enabled',
           authentication: {
           symmetricKey: {
-            primaryKey: new Buffer(uuid.v4()).toString('base64'),
+            primaryKey: pkey,
             secondaryKey: new Buffer(uuid.v4()).toString('base64')
           }
-        }
+        },
+        connectionString: 'HostName=' + host + ';DeviceId=' + deviceId + ';SharedAccessKey=' + pkey
       };
 
       var registry = Registry.fromConnectionString(hubConnectionString);
@@ -59,10 +62,7 @@ var runTests = function (hubConnectionString) {
       registry.create(deviceDescription, function (err) {
         if (err) return done(err);
 
-        var host = ConnectionString.parse(hubConnectionString).HostName;
-        var deviceAccessSignature = deviceSas.create(host, deviceDescription.deviceId, deviceDescription.authentication.symmetricKey.primaryKey, anHourFromNow()).toString();
-
-        deviceClient = deviceSdk.Client.fromSharedAccessSignature(deviceAccessSignature, deviceMqtt);
+        deviceClient = deviceSdk.Client.fromConnectionString(deviceDescription.connectionString, deviceMqtt);
 
         deviceClient.open(function(err) {
           if (err) return done(err);
@@ -81,7 +81,7 @@ var runTests = function (hubConnectionString) {
     });
 
     afterEach(function (done) {
-      this.timeout(20000);
+      this.timeout(10000);
       if (deviceClient) {
         deviceClient.close(function(err) {
           if (err) return done(err);
@@ -134,7 +134,7 @@ var runTests = function (hubConnectionString) {
       });
     });
 
-    it ('sends and receives reported properties', function(done) {
+    var sendsAndReceiveReportedProperties  = function(done) {
       assertObjectIsEmpty(serviceTwin.properties.reported);
       assertObjectIsEmpty(deviceTwin.properties.reported);
 
@@ -146,23 +146,29 @@ var runTests = function (hubConnectionString) {
           done();
         });
       });
-    });
+    };
+  
+    it ('sends and receives reported properties', sendsAndReceiveReportedProperties);
 
-    it ('sends and receives merged reported properties', function(done) {
-      deviceTwin.properties.reported.update(newProps, function(err) {
+    var mergeReportedProperties =  function(first, second, result, done) {
+      deviceTwin.properties.reported.update(first, function(err) {
         if (err) return done(err);
-        deviceTwin.properties.reported.update(moreNewProps, function(err) {
+        deviceTwin.properties.reported.update(second, function(err) {
           if (err) return done(err);
           serviceTwin.get(function(err) {
             if (err) return done(err);
-            assertObjectsAreEqual(mergeResult, serviceTwin.properties.reported);
+            assertObjectsAreEqual(serviceTwin.properties.reported, result);
             done();
           });
         });
       });
+    };
+
+    it ('sends and receives merged reported properties', function(done) {
+      mergeReportedProperties(newProps, moreNewProps, mergeResult, done);
     });
 
-    it ('sends and receives desired properties', function(done) {
+    var sendsAndReceivesDesiredProperties = function(done) {
       assertObjectIsEmpty(deviceTwin.properties.desired);
 
       serviceTwin.update( { properties : { desired : newProps } }, function(err) {
@@ -180,13 +186,21 @@ var runTests = function (hubConnectionString) {
           }
         });
       });
-    });
+    };
 
-    it ('sends and receives merged desired properties', function(done) {
-      serviceTwin.update( { properties : { desired : newProps } }, function(err) {
+    it ('sends and receives desired properties', sendsAndReceivesDesiredProperties);
+      
+    var mergeDesiredProperties = function(first, second, newEtag, result, done) {
+      serviceTwin.update( { properties : { desired : first } }, function(err) {
         if (err) return done(err);
 
-        serviceTwin.update( { properties : { desired : moreNewProps } }, function(err) {
+        if (newEtag) {
+          assert.isDefined(serviceTwin.etag);
+          assert.notEqual(serviceTwin.etag, "*");
+          serviceTwin.etag = newEtag;
+        }
+
+        serviceTwin.update( { properties : { desired : second } }, function(err) {
           if (err) return done(err);
 
           deviceTwin.on('properties.desired', function(props) {
@@ -194,7 +208,7 @@ var runTests = function (hubConnectionString) {
               // ignore
               assert(true);
             } else if (props.$version === 3) {
-              assertObjectsAreEqual(mergeResult, deviceTwin.properties.desired);
+              assertObjectsAreEqual(deviceTwin.properties.desired, result);
               done();
             } else {
               done(new Error('incorrect property version received - ' + props.$version));
@@ -202,31 +216,14 @@ var runTests = function (hubConnectionString) {
           });
         });
       });
+    };
+
+    it ('sends and receives merged desired properties', function(done) {
+      mergeDesiredProperties(newProps, moreNewProps, null, mergeResult, done);
     });
 
     it ('sends and receives merged desired properties using etag *', function(done) {
-      serviceTwin.update( { properties : { desired : newProps } }, function(err) {
-        if (err) return done(err);
-
-        assert.isDefined(serviceTwin.etag);
-        assert.notEqual(serviceTwin.etag, "*");
-        serviceTwin.etag = "*";
-
-        serviceTwin.update( { properties : { desired : moreNewProps } }, function(err) {
-          if (err) return done(err);
-          deviceTwin.on('properties.desired', function(props) {
-            if (props.$verion === 1 || props.$version === 2) {
-              // ignore
-              assert(true);
-            } else if (props.$version === 3) {
-              assertObjectsAreEqual(mergeResult, deviceTwin.properties.desired);
-              done();
-            } else {
-              done(new Error('incorrect property version received - ' + props.$version));
-            }
-          });
-        });
-      });
+      mergeDesiredProperties(newProps, moreNewProps, "*", mergeResult, done);
     });
 
     it ('can get and set tags', function(done) {
@@ -240,35 +237,66 @@ var runTests = function (hubConnectionString) {
       });
     });
 
-    it ('can merge tags', function(done) {
+    var mergeTags =  function(first, second, newEtag, result, done) {
       assertObjectIsEmpty(serviceTwin.tags);
 
-      serviceTwin.update( { tags : newProps }, function(err) {
+      serviceTwin.update( { tags : first }, function(err) {
         if (err) return done(err);
-        serviceTwin.update( { tags: moreNewProps }, function(err) {
+
+        if (newEtag) {
+          assert.isDefined(serviceTwin.etag);
+          assert.notEqual(serviceTwin.etag, "*");
+          serviceTwin.etag = newEtag;
+        }
+
+        serviceTwin.update( { tags: second }, function(err) {
           if (err) return done(err);
-          assertObjectsAreEqual(mergeResult, serviceTwin.tags);
+          assertObjectsAreEqual(serviceTwin.tags, result);
           done();
         });
       });
+    };
+
+    it ('can merge tags', function(done) {
+      mergeTags(newProps, moreNewProps, null, mergeResult, done);
     });
 
     it('can merge tags using etag *', function(done) {
-      assertObjectIsEmpty(serviceTwin.tags);
+      mergeTags(newProps, moreNewProps, "*", mergeResult, done);
+    });
 
-      serviceTwin.update( { tags : newProps }, function(err) {
-        if (err) return done(err);
-
-        assert.isDefined(serviceTwin.etag);
-        assert.notEqual(serviceTwin.etag, "*");
-        serviceTwin.etag = "*";
-
-        serviceTwin.update( { tags: moreNewProps }, function(err) {
-          if (err) return done(err);
-          assertObjectsAreEqual(mergeResult, serviceTwin.tags);
-          done();
-        });
+    it.skip('can send reported properties to the service after renewing the sas token', function(done) {
+      deviceClient.on('_sharedAccessSignatureUpdated', function() {
+        sendsAndReceiveReportedProperties(done);
       });
+      deviceClient._renewSharedAccessSignature();
+    });
+
+    it.skip('can receive desired properties from the service after renewing the sas token', function(done) {
+      deviceClient.on('_sharedAccessSignatureUpdated', function() {
+        sendsAndReceivesDesiredProperties(done);
+      });
+      deviceClient._renewSharedAccessSignature();
+    });
+
+    it.skip('call null out reported properties', function(done) {
+      mergeReportedProperties(newProps, null, {}, done);
+    });
+    
+    it.skip('can null out desired properties', function(done) {
+      mergeDesiredProperties(newProps, null, null, {}, done);
+    });
+
+    it.skip('can null out desired properties with etag *', function(done) {
+      mergeDesiredProperties(newProps, null, "*", {}, done);
+    });
+
+    it.skip('can null out tags', function(done) {
+      mergeTags(newProps, null, null, {}, done);
+    });
+
+    it.skip('can null out tags with etag *', function(done) {
+      mergeTags(newProps, null, "*", {}, done);
     });
 
   });
