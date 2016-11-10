6b2e1edf7a15ae62de264ae924b1fb1a36166600
Haitham Shami <hshami@microsoft.com>
2016-11-10 10:26:32 -0800

    fix project files list

--- csharp/device/Microsoft.Azure.Devices.Client/MethodStatusType.cs
+++ csharp/device/Microsoft.Azure.Devices.Client/MethodStatusType.cs
@@ -4,7 +4,7 @@
 namespace Microsoft.Azure.Devices.Client
 {
     /// <summary>
-    /// Specifies the different acknowledgement levels for DeviceMethod calls.
+    /// Specifies the different acknowledgement levels for Method calls.
     /// </summary>
     public enum MethodStatusType
     {
