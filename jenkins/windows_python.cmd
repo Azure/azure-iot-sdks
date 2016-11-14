95b3edbd404a7ec9462f0d502798f77ed1aa3c44
Zoltan Varga <zolvarga@microsoft.com>
2016-11-14 11:45:34 -0800

    Always use Cmake

--- jenkins/windows_python.cmd
+++ jenkins/windows_python.cmd
@@ -10,12 +10,8 @@ for %%i in ("%build-root%") do set build-root=%%~fi
 
 REM -- Python --
 cd %build-root%\python\build_all\windows
-if "%1" equ "--use-cmake" (
-   echo Building client using cmake
-   call build_client.cmd %2 %3
-) else (
-  echo Building client using Nuget packages
-  call build.cmd --run-ut
-)
+echo Building client using cmake
+call build_client.cmd %2 %3
+
 if errorlevel 1 exit /b 1
 cd %build-root%
