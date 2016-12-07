b0eb9bde54c29c8840d0da2e7518c902ac921cf0
Pierre Cauchois <pierreca@microsoft.com>
2016-12-06 17:28:10 -0800

    Remove jenkins folder since all jenkins folders are now in submodules

--- jenkins/mbed_c.cmd
+++
@@ -1,4 +0,0 @@
-@REM  Copyright (c) Microsoft. All rights reserved.
-@REM  Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-call c\build_all\mbed\build.cmd %*
